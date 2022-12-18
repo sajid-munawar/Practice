5b1a26aa5a62ff8a023a72391accb4c6dff4518e
sajid-munawar <sajidmunawar666@gmail.com>
2022-12-18 15:05:05 +0500

    Extend and super

--- OOP/Dec_18_lhr_class/index.js
+++ OOP/Dec_18_lhr_class/index.js
@@ -1,5 +1,5 @@
 class Point {
-    x = 0;
+    x;
     y = 0;
 }
 ;
@@ -7,4 +7,14 @@ const pt = new Point();
 pt.x = 8;
 pt.y = 10;
 console.log(pt.x, pt.y);
+class Greater {
+    x;
+    y;
+    constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+    }
+}
+let g = new Greater(100);
+console.log(g.x);
 export {};
--- OOP/Dec_18_lhr_class/index.ts
+++ OOP/Dec_18_lhr_class/index.ts
@@ -1,10 +1,42 @@
 class Point {
-    x:number=0;
-    y:number=0;
+    x!: number;
+    y: number = 0;
 };
 
-const pt=new Point();
-pt.x=8;
-pt.y=10;
+const pt = new Point();
+pt.x = 8;
+pt.y = 10;
 
-console.log(pt.x,pt.y);
+console.log(pt.x, pt.y);
+
+class Greater {
+    x: number;
+    y: number;
+
+    constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+let g = new Greater(100)
+console.log(g.x);
+
+class Base {
+    x?:number;
+    constructor(x?:number){
+        this.x=x;
+    }
+}
+
+class Derived extends Base {
+    y:number;
+    constructor(y:number){
+        super();
+        this.y=y;
+    }
+
+}
+
+let d=new Derived(5);
+d.
